ae30c7364ec931a23086f73ebfa4ccbf2810dbc6
aishwaryastudyit-commits <aishwaryastudyit@gmail.com>
2026-08-28 18:03:53 +0530

    Restore consumer marketplace route and styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import Marketplace from "./apps/consumer/pages/Marketplace";
+
 import Orders from "./pages/ajay/Orders";
 import OrderDetails from "./pages/ajay/OrderDetails";
 import Payment from "./pages/ajay/Payment";
@@ -10,13 +12,23 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Navigate to="/orders" replace />} />
+				{/* Default page */}
+				<Route path="/" element={<Navigate to="/marketplace" replace />} />
+
+				{/* Consumer Marketplace */}
+				<Route path="/marketplace" element={<Marketplace />} />
+
+				{/* Orders */}
 				<Route path="/orders" element={<Orders />} />
 				<Route path="/orders/:orderId" element={<OrderDetails />} />
+
+				{/* Payments */}
 				<Route path="/payment/success" element={<PaymentSuccess />} />
 				<Route path="/payment/failed" element={<PaymentFailed />} />
 				<Route path="/payment/:orderId" element={<Payment />} />
-				<Route path="*" element={<Navigate to="/orders" replace />} />
+
+				{/* Unknown routes */}
+				<Route path="*" element={<Navigate to="/marketplace" replace />} />
 			</Routes>
 		</BrowserRouter>
 	);
